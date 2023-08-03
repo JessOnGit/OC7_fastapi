d97ef631b51426b93995383f03654d9503899573
Jess <jessposkitt@gmail.com>
2023-08-03 14:21:36 +0200

    gunicorn config & req update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 # Startup file, fastapi
 
-python -m uvicorn main:app  --reload
-# python -m gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+# python -m uvicorn main:app  --reload
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
